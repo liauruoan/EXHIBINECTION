ba589ad6b971aaef065404bd71b226ce3d6f2f95
LiauRuoAn <LiauRuoAn@gmail.com>
2021-02-25 23:35:34 +0800

    '修改檔案'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -10,3 +10,13 @@ $(document).ready(function(){
       $('.cartList').slideToggle();
   });
 });
+
+$(document).ready(function () {
+
+  $('.top').click(function(event) { 
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+});
