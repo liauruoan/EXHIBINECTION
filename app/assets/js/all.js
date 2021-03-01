00fafc34ebc789248b317c43f2e5845c27f259b6
LiauRuoAn <LiauRuoAn@gmail.com>
2021-03-01 12:32:49 +0800

    '修改檔案'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,22 +1,25 @@
-$(document).ready(function() {
-  $('.showMenu').on('click', function(e){
-      e.preventDefault();
-      $('body').toggleClass('showMenu-list-show');
+$(document).ready(function () {
+  $('.showMenu').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('showMenu-list-show');
   });
 });
 
-$(document).ready(function(){
-  $('.cart').click(function(){ 
-      $('.cartList').slideToggle();
+$(document).ready(function () {
+  $('.cart').click(function () {
+    $('.cartList').slideToggle();
   });
 });
 
 $(document).ready(function () {
 
-  $('.top').click(function(event) { 
+  $('.top').click(function (event) {
     event.preventDefault();
     $('html,body').animate({
       scrollTop: 0
     }, 1000);
+
+    return false;
   });
+
 });
